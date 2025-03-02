542c88b09cb50e751d63db3215b021293f9ea790
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-03-01 16:03:59 -0800

    implemented start page

---
+++ athenahacks/src/components/StartPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/startpage.css'
+
+function StartPage() {    
+    return(
+        <>
+            <div className="startpage-container pixelify-sans">
+                <h1 className="startpage-title">PassionFinder</h1>
+                <button className="start-button pixelify-sans">Start!</button>
+            </div>
+        </>
+    );
+}
+
+export default StartPage
