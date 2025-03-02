56f0db01b595c7d0938000698f650f2b6aa1d8d4
katieyungchung <katieyungchung@g.ucla.edu>
2025-03-01 18:58:05 -0800

    Add hobby page

--- athenahacks/src/App.jsx
+++ athenahacks/src/App.jsx
@@ -3,8 +3,10 @@ import StartPage from './components/StartPage.tsx';
 import WelcomePage from './components/WelcomePage.jsx';
 import QuestionPage from './components/QuestionPage.jsx';
 import DiscoveryQuiz from './components/DiscoveryQuiz.jsx';
+import HomePage from './components/HomePage.jsx';
 import ResultsPage from './components/ResultsPage';
 import EndPage from './components/EndPage';
+import AddHobby from './components/AddHobby.jsx';
 import {Routes, Route} from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -16,7 +18,9 @@ function App() {
         <Route path="/welcomepage" element={<WelcomePage/>}/>
         <Route path="/questionpage" element={<QuestionPage/>}/>
         <Route path="/discoveryquiz" element={<DiscoveryQuiz/>}/>
+        <Route path="/addhobby" element={<AddHobby/>}/>
         <Route path="/resultspage" element={<ResultsPage/>}/>
+        <Route path="/homepage" element={<HomePage/>}/>
         <Route path="/endpage" element={<EndPage/>}/>
         
       </Routes>
---
+++ athenahacks/src/components/AddHobby.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../css/AddHobby.css';
+
+function AddHobby() {
+  const [hobbyName, setHobbyName] = useState('');
+  const [image, setImage] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('name', hobbyName);
+    formData.append('image', image);
+
+    try {
+      const response = await axios.post('/api/hobbies', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log('Hobby added:', response.data);
+    } catch (error) {
+      console.error('Error adding hobby:', error);
+    }
+  };
+
+  return (
+    <div className="pixelify-sans">
+      <h1>Add a New Hobby</h1>
+      <form onSubmit={handleSubmit} id="hobby-form">
+        <div>
+          <label htmlFor="hobbyName">Hobby Name:</label>
+          <input
+            type="text"
+            id="hobbyName"
+            value={hobbyName}
+            onChange={(e) => setHobbyName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="img-upload">
+          <label htmlFor="image">Upload Image:</label>
+          <input
+            type="file"
+            id="image"
+            accept="image/*"
+            onChange={(e) => setImage(e.target.files[0])}
+            required
+          />
+        </div>
+        <div className="center-button">
+            <button type="submit">Add Hobby</button>
+        </div>
+       
+      </form>
+    </div>
+  );
+}
+
+export default AddHobby;
