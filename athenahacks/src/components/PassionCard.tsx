adc30073699946d3d866b3c7102693d9d517b6e2
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-03-01 20:43:19 -0800

    created new passion card

---
+++ athenahacks/src/components/PassionCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props{
+    name: string;
+    description: string;
+}
+
+function PassionCard(passion: Props){
+
+    return (
+        <>
+        <div className="passioncard-container">
+
+        </div>
+        </>
+    );
+}
+
+export default PassionCard
