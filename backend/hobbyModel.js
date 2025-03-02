0cb04d0992c5d18d4c6b5bc5a720faff7542b440
katieyungchung <katieyungchung@g.ucla.edu>
2025-03-02 12:19:25 -0800

    update weeklytimespent values

--- backend/hobbyModel.js
+++ backend/hobbyModel.js
@@ -4,7 +4,7 @@ const HobbySchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String }, // Store image URL
   totalTimeSpent: { type: Number, default: 0 },
-  weeklyTimeSpent: { type: [Number], default: Array(7).fill(0) },
+  weeklyTimeSpent: { type: [Number], default: Array(7).fill(10) },
   notes: { type: String },
   additionalInfo: { type: String } // AI-generated info
 });
